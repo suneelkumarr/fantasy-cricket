a20fb35e3de4440d6bb1b9f7a7aec9ba13b7b13b
suneelkumarr <sunilkumar05644@gmail.com>
2025-03-21 16:27:49 +0530

    added 2nd build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'fantasy-cricket',
   plugins: [react()],
   server: {
     allowedHosts: [
