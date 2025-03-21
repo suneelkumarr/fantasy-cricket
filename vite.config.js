b20db2d49e9c1795e731762a47f1bcf09ef80621
suneelkumarr <sunilkumar05644@gmail.com>
2025-03-21 16:28:39 +0530

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'fantasy-cricket',
+  base: '/fantasy-cricket',
   plugins: [react()],
   server: {
     allowedHosts: [
