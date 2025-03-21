96371bb72842d0d88baa8a5027348103a3e3dcf6
suneelkumarr <sunilkumar05644@gmail.com>
2025-03-21 17:17:24 +0530

    added new variaable

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fantasy-cricket/',
+  base: import.meta.env.VITE_BASE_PATH,
   plugins: [react()],
   server: {
     allowedHosts: [
