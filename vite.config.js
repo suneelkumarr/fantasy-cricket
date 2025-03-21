31a962c8a722e57434e47eda7fef34cf50029275
suneelkumarr <sunilkumar05644@gmail.com>
2025-03-21 17:18:43 +0530

    added padth

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_PATH,
+  base: "/fantasy-cricket",
   plugins: [react()],
   server: {
     allowedHosts: [
