72440dfc8c456b7a7c2930a188f91fd580f31530
suneelkumarr <sunilkumar05644@gmail.com>
2025-03-21 17:31:00 +0530

    aded new domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fantasy-cricket",
+  base: "/",
   plugins: [react()],
   server: {
     allowedHosts: [
